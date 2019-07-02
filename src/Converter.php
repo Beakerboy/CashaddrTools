df80ec639bbf054a77c5485593ee33c901586678
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-01 20:20:00 -0400

    Update Converter.php

--- src/Converter.php
+++ src/Converter.php
@@ -1,4 +1,4 @@
-php
+<?php
 
 namespace CashaddrTools;
 
@@ -42,8 +42,26 @@ class Converter
     {
     }
 
+    /**
+     * isCashaddr
+     *
+     * Check if an address conforms to the Cashaddr standard
+     *
+     * @param string $address
+     *   bitcoin address
+     *
+     * @returns boolean
+     */
     public static function isCashaddr($address)
     {
+        // Must be all upper or all lower case.
+        if (strtolower($address) !== $address && strtoupper($address) !== $address) {
+            return false;
+        }
+        $address = strtolower($address);
+
+        // Addres has an optional prefix, which must have a colon after it followed by the payload.
+        // The payload must start with a p or q and be followed by the set of base32 alphanumerics.
         $regex = '/^((bitcoincash|bchtest|bchreg):?)[qp]{1}[02-9ac-hj-np-z]{41}$/';
         return preg_match($regex, $address);
     }
