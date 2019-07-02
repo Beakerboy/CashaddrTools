5827c45d11e3d9d5a9fc7cdd7f888256a5fd7928
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-02 19:35:18 -0400

    Update TestConverter.php

--- tests/TestConverter.php
+++ tests/TestConverter.php
@@ -1,8 +1,18 @@
 <?php
 namespace CashaddrTools;
 
+/**
+ * TestConverter
+ *
+ * This class exposes protected classes in Converter to allow unit testing
+ */
 class TestConverter extends Converter
 {
+    public static function getByteArray($x)
+    {
+        return parent::getByteArray($x);
+    }
+
     public static function polymod($x)
     {
         return parent::polymod($x);
