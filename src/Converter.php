117b1c62b177d69eb0cf0e00b339e4b7a0707c09
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-02 20:56:20 -0400

    Update Converter.php

--- src/Converter.php
+++ src/Converter.php
@@ -122,7 +122,7 @@ class Converter
      * @param string @address
      * @returns string in base16 encoding
      */
-    public function getHash($address): string
+    public static function getHash($address): string
     {
         $hex_codes = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
         $payload = self::getPayload($address);
