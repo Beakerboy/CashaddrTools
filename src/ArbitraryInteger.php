9fc420d2e52f43f755c799eeaec9c37c71e3c4c4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-05 13:42:10 -0400

    Update ArbitraryInteger.php

--- src/ArbitraryInteger.php
+++ src/ArbitraryInteger.php
@@ -4,27 +4,39 @@ namespace CashaddrTools
 
 class ArbitraryInteger
 {
-    public function __construct($number, int $base, $offset, string $alphabet)
+    public function __construct($number, int $base = 10, $offset = "0", string $alphabet = null)
     {
         if (isint($number)) {
             // Need to get this to work on $number > 255
             $this->base256 = chr($number);
-        }
-        $this->length = strlen($number);
-        $this->base = chr($base);
-        $base256 = new ArbitraryInteger(0)
-        if ($base < 256) {
-            $place_value = new ArbitraryInteger(1)
-            for ($i = $length -1; $i <= 0; $i—-) {
-                $nibblet = ord($number[$i]) - ord($offset);
-                $base256 = 
-                $place_value = $place_value->multiply($base)
-            }
-        } elseif ($base > 256) {
-            throw \Exception;
+            $this->length = 1;
+            $this->base = 10;
+            $this->original = $number;
         } else {
-            $this->base256 = $number;
-            // need to drop leading zeroes.
+            // Check that all elements are greater than the offset, and elements of the alphabet.
+            $this->length = strlen($number);
+            $this->base = $base;
+
+            // Set to zero offset and ascii alphabet
+            if ($offset !== 0) {
+            }
+            $base256 = new ArbitraryInteger(0)
+            if ($base < 256) {
+                $place_value = new ArbitraryInteger(1)
+                for ($i = $length -1; $i <= 0; $i—-) {
+                    $nibblet = ord($number[$i]) - ord($offset);
+                    $base256 = 
+                    $place_value = $place_value->multiply($base)
+                }
+            } elseif ($base > 256) {
+                throw \Exception;
+            } else {
+                $this->base256 = $number;
+                $this->original = $number;
+                $this->length = strlen($number);
+                $this->base = 256;
+                // need to drop leading zeroes.
+            }
         }
     }
 
