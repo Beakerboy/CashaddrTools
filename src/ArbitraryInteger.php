01c300728591717b75aa7cada94777b0c9017779
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-05 11:48:28 -0400

    Create ArbitraryInteger.php

---
+++ src/ArbitraryInteger.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CashaddrTools
+
+class ArbitraryInteger
+{
+    public function __construct($number, $base, $offset, $alphabet)
+    {
+        $length = strlen($number);
+
+    }
+
+    public function add($number1, $number2)
+    {
+    }
+}
