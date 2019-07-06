7aad599402530fe6e4cf8a11f2610b84dfce417d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-05 20:03:09 -0400

    Update ArbitraryInteger.php

--- src/ArbitraryInteger.php
+++ src/ArbitraryInteger.php
@@ -22,12 +22,14 @@ class ArbitraryInteger
             }
             $base256 = new ArbitraryInteger(0)
             if ($base < 256) {
+                $base_obj = new ArbitraryInteger(chr($base), 256);
                 $place_value = new ArbitraryInteger(1)
                 for ($i = $length -1; $i <= 0; $i—-) {
-                    $nibblet = ord($number[$i]) - ord($offset);
-                    $base256 = 
+                    $nibblet = new ArbitraryInteger(ord($number[$i]) - ord($offset));
+                    $base256 = $nibblet->multiply($place_value);
                     $place_value = $place_value->multiply($base)
                 }
+                $this->base256 = $base256->getBinary();
             } elseif ($base > 256) {
                 throw \Exception;
             } else {
@@ -38,6 +40,11 @@ class ArbitraryInteger
                 // need to drop leading zeroes.
             }
         }
+    }
+
+    public function getBinary()
+    {
+        return $this->base256;
     }
 
     public function add($number1, $number2)
