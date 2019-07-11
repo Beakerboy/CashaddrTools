2c45525fe53080deedc04fcdcc9f1ae2e2860e3c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-10 22:40:11 -0400

    Update ConverterTest.php

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -41,6 +41,13 @@ class ConverterTest extends \PHPUnit\Framework\TestCase
         ];
     }
 
+    public function testToCashaddr()
+    {
+        $expected = 'bitcoincash:qpm2qsznhks23z7629mms6s4cwef74vcwvy22gdx6a';
+        $legacy = '1BpEi6DfDAUFd7GtittLSdBeYJvcoaVggu';
+        $this->assertEquals($expected, CashaddrTools::toCashaddr($legacy));
+    }
+
     /**
      * @testCase testIsNotCashaddr()
      * @dataProvider dataProviderForTestIsNotCashaddr
